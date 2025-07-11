182dedcdfd5ac53f7a7e5b9c7dd0ad312ca17097
박준영 <joonyeong.park@dearu.com>
2025-07-11 11:43:56 +0900

    feat: 기본 패키지 구성, gradle 추가

---
+++ four-tier-architecture/src/main/java/com/example/fourtier/FourTierApplication.java
@@ -0,0 +1,12 @@
+package com.example.fourtier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourTierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FourTierApplication.class, args);
+    }
+}

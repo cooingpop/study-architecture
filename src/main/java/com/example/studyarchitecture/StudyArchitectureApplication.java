f9d0d438cf357c8275f9b8786d2b55fd662d377b
박준영 <joonyeong.park@dearu.com>
2025-07-11 09:54:23 +0900

    feat: 최초 프로젝트 초기화

---
+++ src/main/java/com/example/studyarchitecture/StudyArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyArchitectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studyarchitecture/StudyArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
